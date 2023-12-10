52babf5cadf93e1c4d177f6760684f1612f14da4
NgtNhat-131101 <nguyentiennhat2k1@gmail.com>
2023-12-10 22:53:43 +0700

    10Dec

--- 07_pointer_2/main.c
+++ 07_pointer_2/main.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+
+int main()
+{
+    int value = 5;
+    int *pt = &value;
+    int **p2p = &pt;
+    
+    printf("&value = %p\n", (void *)&value);
+
+    // Print the value of the pointer `pt` (address it holds)
+    printf("pt = %p\n", (void *)pt);
+
+    // Print the address of the pointer `pt`
+    printf("&pt = %p\n", (void *)&pt);
+
+    // Print the value of the pointer `p2p` (address it holds, which is the address of `pt`)
+    printf("p2p = %p\n", (void *)p2p);
+
+    // Print the value of the pointer `pt` (address it points to, the address of `value`)
+    printf("*p2p = %p\n", (void *)*p2p);
+
+    // Print the value stored at the address pointed to by `pt` (value of `value`)
+    printf("**p2p = %d\n", **p2p);
+
+    return 0;
+}
---
+++ 07_pointer_2/note_pointer_lv2.md
@@ -0,0 +1,35 @@
+### Pointer Level 2
+
+#### Recall Pointer 1:
+
+- For example:
+
+        
+        int value = 5;
+        int *pt = &value;
+        
+    > In this case: `*pt is 5` and `pt is address of "value"`
+
+#### Pointer 2:
+- We need to modify value of register that pointer points to \> So we need to use pointer level 2
+
+- Continue the above code:
+
+        **pointer_to_pointer = &pt;
+        `pointer_to_pointer points to address of pt`
+    
+<center>
+
+![Local Image](p2p.png)
+
+</center>
+
+- The purposes of a level-2 pointer are:
+
+    1. Modify the value that the pointer points to.
+
+    2. Change the address of the pointer.
+
+
+`16:50`
+
