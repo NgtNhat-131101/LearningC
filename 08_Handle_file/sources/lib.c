09789a4a05b354b5a4226817011c286fa354036d
NgtNhat-131101 <nguyentiennhat2k1@gmail.com>
2023-12-18 22:21:57 +0700

    18Dec

---
+++ 08_Handle_file/sources/lib.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+char UART_Rx[100];
+
---
+++ 08_Handle_file/sources/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+void processingUART(char *data, int lenght)
+{
+    printf("Data UART: %s\n", data);
+
+    if (strstr(data, "ON"))
+    {
+        printf("Turn on LED\n");
+    }
+    else if (strstr(data, "OFF"))
+    {
+        printf("Turn off LED");
+    }
+}
+
+void processingUART_2(char *data, int len)
+{
+    printf("Data UART: %s\n", data);
+
+    if (strstr(data, "BLINK"))
+    {
+        printf("Blink LED\n");
+    }
+    else if (strstr(data, "TOGGLE"))
+    {
+        printf("Toggle LED");
+    }
+}
+
+int main()
+{
+    return 0;
+}
